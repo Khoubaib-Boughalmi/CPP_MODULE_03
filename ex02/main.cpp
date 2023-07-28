2b2412af627d988e0e4eca5c3a61e9b85f719818
Khoubaib-Boughalmi <khoubaib.kb99@gmail.com>
2023-07-28 15:26:08 +0100

    finished ex00 ex01 ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -2,11 +2,13 @@
 
 int main()
 {
+    
     FragTrap fragTrap("FragTrapNinja");
     fragTrap.attack("Ninja Slayer");
     fragTrap.takeDamage(5);
     fragTrap.beRepaired(12);
     fragTrap.takeDamage(77);
     fragTrap.highFivesGuys();
+
     return (0);
 }
